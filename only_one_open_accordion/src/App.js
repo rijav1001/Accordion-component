93204f06427f0810389b95b80dd339f772b3c3ea
rijav1001 <rvu100199@gmail.com>
2022-01-08 22:49:43 +0530

    accordion one open done

---
+++ only_one_open_accordion/src/App.js
@@ -0,0 +1,14 @@
+import Accordion from './components/Accordion.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='App'>
+      <Accordion />
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ only_one_open_accordion/src/components/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+      id: 1,
+      sample: "This is test1",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,"
+    },
+    {
+      id: 2,
+      sample: "This is test2",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,"
+    },
+    {
+      id: 3,
+      sample: "This is test3",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,"
+    }
+  ]
